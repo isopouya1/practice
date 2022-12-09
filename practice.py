d752a35b944a3b0ae8ee49680e17b86a91918520
isopouya <pouya2402@gmail.com>
2022-12-09 11:21:41 +0330

    AddNew

---
+++ practice.py
@@ -0,0 +1,51 @@
+# third practice
+# litr = int(input("litr : "))
+# tabdil = litr * 950
+# javab = tabdil // 3
+# print(f"geram : {tabdil} \nmolkol : {javab}")
+
+
+# fourh practice
+# cash = int(input("your cash out : "))
+# calculatur = cash // 100
+# bime = calculatur * 7
+# maliat = calculatur * 10
+# jam = cash - maliat - bime
+# print(f"hoghog : {cash}  maliat : {maliat}  bime : {bime}  dariaft : {jam}")
+
+
+# fivth practice
+# student_marks = []
+# student_marks.append(float(input("first student : ")))
+# student_marks.append(float(input("second student : ")))
+# student_marks.append(float(input("third student : ")))
+# student_marks.insert(0,19.5)
+# print(student_marks)
+
+
+# sixth practice
+# numbers = []
+# num1 = int(input("first number : "))
+# num2 = int(input("second number : "))
+# num3 = int(input("third number : "))
+# num4 = int(input("forth number : "))
+# numbers.append(num1)
+# numbers.append(num2)
+# numbers.append(num3)
+# numbers.append(num4)
+# result = (num1 + num2 + num3 + num4) / 4
+# numbers.append(result)
+# print(numbers)
+
+
+# seventh practice
+# letter = input("Enter name : ")
+# stringlength = len(letter)
+# reversestiring = letter[stringlength::-1]
+# print(reversestiring)
+
+
+# eightsh practice 
+# fame = "pouya zohrehvand said :".title()
+# speak = "Live for ourselves not for showing that to others"
+# print(f"{fame} \n{speak}")
